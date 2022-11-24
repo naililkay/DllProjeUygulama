9206384c8ba3d4ffaacf5a3965196f491757d8c2
Nail <Nail@USER-NAL>
2022-11-24 09:25:55 +0300

    Add project files.

---
+++ DllProjeUygulama/Personel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllProjeUygulama
+{
+    internal class Personel
+    {
+    //    public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+     //   public string Cinsiyet { get; set; }
+     //   public DateTime DogumTarihi { get; set; }
+    //    public string Cadde { get; set; }
+       // public int KapiNo { get; set; }
+      //  public string Ilce { get; set; }
+        public string Sehir { get; set; }
+     //   public int Maas { get; set; }
+
+
+
+    }
+}
---
+++ DllProjeUygulama/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System;
+using DllProjeUygulama;
+using MyFakeData;
+
+List<Personel> Perlist = new List<Personel>();
+
+
+//while (Console.ReadKey(true).KeyChar == 'q')
+//{
+//    for (int i = 1; i < 21; i++)
+//    {
+
+//        Personel personel = new Personel();
+
+//        personel.Ad = MyFakeData.NameData.GetFirstName();
+//        personel.Soyad = MyFakeData.NameData.GetLastName();
+//        personel.Sehir = MyFakeData.PlaceData.GetCountryName();
+
+//        Perlist.Add(personel);
+//    }
+
+//    int k = 1;
+//    foreach (var item in Perlist)
+//        Console.WriteLine($"{k++} {item.Ad} {item.Soyad} {item.Sehir}");
+//}
+
+do
+{
+    Perlist.Clear();
+    for (int i = 1; i < 21; i++)
+    {
+
+
+        Personel personel = new Personel();
+
+        personel.Ad = MyFakeData.NameData.GetFirstName();
+        personel.Soyad = MyFakeData.NameData.GetLastName();
+        personel.Sehir = MyFakeData.PlaceData.GetCountryName();
+
+        Perlist.Add(personel);
+    }
+
+    int k = 1;
+    foreach (var item in Perlist)
+        Console.WriteLine($"{k++} {item.Ad} {item.Soyad} {item.Sehir}");
+    Console.WriteLine();
+
+} while (Console.ReadKey(true).KeyChar != 'q');
